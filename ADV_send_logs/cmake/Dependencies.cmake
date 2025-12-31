63a9826b9112ce39a38e70920573cc85a24ddcc5
krissakm <martin.krissak@advacam.cz>
2025-12-31 09:23:16 +0100

    add debug cmake messages

--- ADV_send_logs/cmake/Dependencies.cmake
+++ ADV_send_logs/cmake/Dependencies.cmake
@@ -1,5 +1,7 @@
 # Dependencies.cmake
 
+message("Dependencies.cmake")
+
 if(USE_SYSTEM_CURL)
     find_package(CURL)
     if(CURL_FOUND)
@@ -14,6 +16,7 @@ endif()
 if(NOT USE_SYSTEM_CURL)
     # Přidáme jen to, co je potřeba, z 3rdparty/curl
     # např. jen statickou knihovnu curl (bez testů, docs, tools)
+    message(STATUS "Settings set to not use system-installed libcurl")
     add_subdirectory(3rdparty/curl EXCLUDE_FROM_ALL)
     set(CURL_TARGET curl)  # podle jména targetu v CMakeLists.txt 3rdparty/curl
 endif()
